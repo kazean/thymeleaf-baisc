aa887789c5a839c7b195501345c8b3abac81913a
kazean <yeogunna@gmail.com>
2022-01-30 22:33:48 +0900

    Initial commit

---
+++ src/main/java/hello/thymeleafbaisc/ThymeleafBaiscApplication.java
@@ -0,0 +1,13 @@
+package hello.thymeleafbaisc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafBaiscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafBaiscApplication.class, args);
+	}
+
+}
